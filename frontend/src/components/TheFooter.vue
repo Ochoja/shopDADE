e8a7d0b5b66c1b2765f33596a849a113ab5cf299
Daniel Ochoja <danielochoja@yahoo.com>
2024-03-21 11:56:39 +0100

    implemented footer

---
+++ frontend/src/components/TheFooter.vue
@@ -0,0 +1,83 @@
+<script setup>
+import { Icon } from '@iconify/vue'
+</script>
+
+<template>
+  <footer>
+    <div class="items">
+      <div class="brand">
+        <div class="logo">shopDADE</div>
+        <p>
+          Discover our latest arrivals, curated for the modern working class, shop now to elevate
+          your wardrobe with fresh sophistication and style.
+        </p>
+      </div>
+      <div class="links">
+        <h3>Collections</h3>
+        <div>Suit</div>
+        <div>Footwear</div>
+        <div>Accesories</div>
+        <div>Tops</div>
+        <div>Bottoms</div>
+      </div>
+      <div class="links">
+        <h3>Company</h3>
+        <div>Privacy Policy</div>
+        <div>Terms of Service</div>
+        <div>Customer Care</div>
+      </div>
+      <div class="icons">
+        <a href="https://github.com/Ochoja/shopDADE">
+          <Icon icon="flowbite:x-solid"></Icon>
+        </a>
+        <a href="https://github.com/Ochoja/shopDADE">
+          <Icon icon="mdi:instagram"></Icon>
+        </a>
+        <a href="https://github.com/Ochoja/shopDADE">
+          <Icon icon="ant-design:facebook-outlined"></Icon>
+        </a>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss">
+footer {
+  color: #fff;
+  background: $primary-color;
+  padding: 20px 0 8px 20px;
+  font-weight: 300;
+
+  .items {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    gap: 10px;
+    @include grid-width;
+
+    .brand {
+      max-width: 24.5em;
+
+      p {
+        font-size: 0.8em;
+      }
+    }
+
+    .links {
+      * {
+        margin-bottom: 5px;
+      }
+    }
+
+    .icons {
+      display: flex;
+      gap: 8px;
+
+      a {
+        color: #fff;
+        font-size: 1.8em;
+      }
+    }
+  }
+}
+</style>
