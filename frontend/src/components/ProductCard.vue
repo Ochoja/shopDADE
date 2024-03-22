a70b0c213e3daa26755e15eabbf101c1ff0fcfa8
Daniel Ochoja <danielochoja@yahoo.com>
2024-03-22 11:31:56 +0100

    updated

---
+++ frontend/src/components/ProductCard.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+defineProps({
+  product: Object
+})
+</script>
+
+<template>
+  <RouterLink to="/" class="product">
+    <div class="img">
+      <img :src="product.image" alt="Product_Image" />
+    </div>
+    <div class="name">{{ product.product_name }}</div>
+    <div class="price">${{ product.price }}</div>
+  </RouterLink>
+</template>
+
+<style lang="scss" scoped>
+.product {
+  text-decoration: none;
+  text-align: left;
+  color: #000;
+  font-size: 1.1em;
+  display: flex;
+  flex-direction: column;
+
+  .img {
+    object-fit: cover;
+    margin-bottom: 14px;
+
+    img {
+      border-radius: 10px;
+      object-fit: contain;
+      height: auto;
+      width: inherit;
+    }
+  }
+
+  .name {
+    font-weight: 600;
+    // font
+  }
+}
+</style>
