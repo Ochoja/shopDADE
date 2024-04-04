27e80ebbfb9044d376cf0d05f0542de1e1f4c848
niyioo <adeniyi@cybersecinsight.com>
2024-04-05 00:30:57 +0100

    updated products model and resource, also schemas, all CRUD working now

--- backend/resources/product_resources.py
+++ backend/resources/product_resources.py
@@ -1,13 +1,14 @@
 from flask import jsonify
 from flask_restful import Resource, reqparse
 from models.product import Product
+from schemas import ProductSchema
+
+product_schema = ProductSchema()
 
 class ProductListResource(Resource):
     def get(self):
         products = Product.find_all()
-        serialized_products = [{'_id': str(product['_id']), 'title': product['title'], 'description': product['description'],
-                                'price': product['price'], 'stock_quantity': product['stock_quantity'],
-                                'category_id': product['category_id'], 'image_url': product['image_url']} for product in products if 'category_id' in product]
+        serialized_products = [product_schema.dump(product) for product in products]
         return jsonify({'products': serialized_products})
 
     def post(self):
@@ -15,6 +16,7 @@ class ProductListResource(Resource):
         parser.add_argument('title', type=str, required=True)
         parser.add_argument('description', type=str)
         parser.add_argument('price', type=float, required=True)
+        parser.add_argument('old_price', type=float)
         parser.add_argument('stock_quantity', type=int, default=0)
         parser.add_argument('category_id', type=str, required=True)
         parser.add_argument('image_url', type=str)
@@ -25,26 +27,26 @@ class ProductListResource(Resource):
             return {'message': 'Product with the same title already exists'}, 400
 
         product = Product(title=args['title'], description=args['description'], price=args['price'],
+                          old_price=args.get('old_price'),
                           stock_quantity=args['stock_quantity'], category_id=args['category_id'], image_url=args['image_url'])
 
         product.save()
 
-        return {'_id': str(product['_id']), 'title': product['title'], 'description': product['description'],
-                'price': product['price'], 'stock_quantity': product['stock_quantity'],
-                'category_id': product.get('category_id'), 'image_url': product.get('image_url')}, 201
+        serialized_product = product_schema.dump(product)
+        return serialized_product, 201
 
 class ProductResource(Resource):
     def get(self, product_id):
         product = Product.find_by_id(product_id)
-        return {'product': {'_id': str(product['_id']), 'title': product['title'], 'description': product['description'], 
-                            'price': product['price'], 'stock_quantity': product['stock_quantity'], 
-                            'category_id': product.get('category_id'), 'image_url': product.get('image_url')}}
+        serialized_product = product_schema.dump(product)
+        return serialized_product
 
     def put(self, product_id):
         parser = reqparse.RequestParser()
         parser.add_argument('title', type=str)
         parser.add_argument('description', type=str)
         parser.add_argument('price', type=float)
+        parser.add_argument('old_price', type=float)
         parser.add_argument('stock_quantity', type=int)
         parser.add_argument('category_id', type=str)
         parser.add_argument('image_url', type=str)
