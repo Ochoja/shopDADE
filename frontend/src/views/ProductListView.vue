d7ad0ae3dd4c3efce59a4aab21c58bb456614d66
Daniel Ochoja <danielochoja@yahoo.com>
2024-04-05 04:33:42 +0100

    updated requirements.txt

---
+++ frontend/src/views/ProductListView.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  featured: String,
+  category: String,
+  search: String
+})
+
+const title = ref('') // page title
+if (props.featured) {
+  title.value = 'Featured'
+}
+</script>
+
+<template>
+  <div class="main">
+    <h1>{{ title }} Products</h1>
+
+    <div class="products"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.main {
+  @include grid-width;
+
+  .products {
+    @include product-grid;
+    margin-bottom: 30px auto;
+  }
+}
+</style>
